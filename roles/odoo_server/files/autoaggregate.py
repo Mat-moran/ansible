22e047c12b7f99f0664203961e14b7c4d638b952
Mat-moran <amadeo.moran@gmail.com>
2021-01-27 11:40:41 +0100

    [UPD] to be able to include private repositories with token acces

--- roles/odoo_server/files/autoaggregate.py
+++ roles/odoo_server/files/autoaggregate.py
@@ -1,6 +1,14 @@
+from sys import argv
 import yaml
 from pprint import pprint
 import re
+
+if len(argv) > 1:
+    tokens = True
+    token_name, token_pass = argv[1], argv[2]
+else:
+    tokens = False
+
 
 exclude_list = ["ENV", "ONLY"]
 
@@ -16,22 +24,25 @@ with open(r"repos.yaml") as file:
 with open(r"addons.yaml") as file:
     a = yaml.load_all(file, Loader=yaml.FullLoader)
     for doc in a:
-        if "ENV" in doc.keys():
-            repoName = doc["ENV"]["DEFAULT_REPO_PATTERN"]
-        else:
-            repoName = "https://github.com/OCA/{}.git"
         print(doc.keys())
         for repo in doc.keys():
             if repo not in repos_dict.keys() and repo not in exclude_list:
-
+                if "ENV" in doc.keys():
+                    repoName = doc["ENV"]["DEFAULT_REPO_PATTERN"]
+                    if tokens:
+                        repoName = repoName.format(token_name, token_pass, repo)
+                    else:
+                        repoName = repoName.format(repo)
+                else:
+                    repoName = "https://github.com/OCA/{}.git".format(repo)
                 repos_dict["./" + repo] = {
                     "defaults": {
                         "depth": "$DEPTH_DEFAULT",
                     },
                     "merges": ["ocb $ODOO_VERSION"],
-                    "remotes": {"ocb": repoName.format(repo)},
+                    "remotes": {"ocb": repoName},
                     "target": "ocb $ODOO_VERSION",
                 }
 
-with open("repos.yml", "w") as outfile:
+with open("repos.yaml", "w") as outfile:
     yaml.dump(repos_dict, outfile, default_flow_style=False)
